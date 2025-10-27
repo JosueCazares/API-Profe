56230ac2df1c276541e20fbdf09a870aec2d6822
JosueCazares <yael.cazares@grupovizer.com>
2025-10-27 11:36:45 -0600

    correcion e implementacion del dao

---
+++ src/dao/PrismaProfesorDao.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../db";
+
+export class ProfesorModel {
+  static async getAll() {
+    return prisma.profesor.findMany();
+  }
+
+  static async getById(numEMpleado: number) {
+    return prisma.profesor.findUnique({ where: { numEMpleado } });
+  }
+
+  static async create(data: any) {
+    return prisma.profesor.create({ data });
+  }
+
+  static async update(numEMpleado: number, data: any) {
+    return prisma.profesor.update({ where: { numEMpleado }, data });
+  }
+
+}
